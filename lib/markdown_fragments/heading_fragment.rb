270bf07d9223a678ddc6875ad86e9a5103048c18
Douglas Pouk <doug@flyonlinetools.com>
2016-03-13 15:29:50 -0500

    improved formatting of headings

--- lib/markdown_fragments/heading_fragment.rb
+++ lib/markdown_fragments/heading_fragment.rb
@@ -3,13 +3,14 @@ class HeadingFragment < MarkdownFragment
 
     def render_on(pdf_object, options = {})
       arguments = _default_render_options.merge(options)
+      pdf_object.move_down 12
       pdf_object.text @content.join(' '), arguments
     end
 
   private
 
     def _default_render_options
-      options = { :size => (24 - (4*@level)), :align => :left, :leading => 2, :weight => :bold, :color=>'444444' }
+      options = { :size => (24 - (4*@level)), :align => :left, :leading => 4, :weight => :bold, :color=>'444444' }
       options.merge({:inline_format => true})
       options
     end
