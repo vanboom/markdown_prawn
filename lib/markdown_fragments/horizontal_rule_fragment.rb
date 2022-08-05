0e07daa409e68f99605b7d2d5e574ce1a9c93f8b
Douglas Pouk <doug@flyonlinetools.com>
2022-08-05 13:16:09 -0500

    0.0.42 reduce width of horizontal rule

--- lib/markdown_fragments/horizontal_rule_fragment.rb
+++ lib/markdown_fragments/horizontal_rule_fragment.rb
@@ -3,7 +3,7 @@ class HorizontalRuleFragment < MarkdownFragment
   def render_on(pdf_object, options = {})
     pdf_object.move_down(3)
     old_width = pdf_object.line_width
-    pdf_object.line_width = 3
+    pdf_object.line_width = 1
     pdf_object.horizontal_rule
     pdf_object.stroke
     pdf_object.line_width = old_width
